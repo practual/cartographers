7110fe3d42fce496fa740fa9ebc536338972081b
Phil Richman <practual@protonmail.com>
2022-05-28 15:15:30 +0100

    Add scorecard labels

--- src/game/scoring.js
+++ src/game/scoring.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 function Scoring({scoringData, onClose}) {
+    const cardLabels = ['A', 'B', 'C', 'D'];
+
     return (
         <>
             <h1>Scorecards</h1>
-            {scoringData.map(scoring => (
+            {scoringData.map((scoring, idx) => (
                 <div key={scoring.id}>
-                    <h2>{scoring.name}</h2>
+                    <h2>{cardLabels[idx]}: {scoring.name}</h2>
                     <p>{scoring.description}</p>
                 </div>
             ))}
